591640e8484b7270bdee437c1904cce09cf672f5
David Shaw <dshaw11@utk.edu>
2024-08-26 13:37:57 -0400

    found and used the recursive function used by net/html to parse an html document and find all of the a tags

--- get_url_from_html.go
+++ get_url_from_html.go
@@ -1,6 +1,36 @@
 package main
 
+import (
+	"strings"
+	"fmt"
+
+	"golang.org/x/net/html"
+)
 
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-	return []string{"", ""}, nil
+	var linkSlice []string
+	doc, err := html.Parse(strings.NewReader(htmlBody))
+	if err != nil {
+		return []string{"", ""}, err
+	}
+	
+	linkSlice = parseNode(doc, linkSlice)
+
+	fmt.Println(linkSlice)
+	return linkSlice, err
+}
+
+func parseNode(n *html.Node, stringSlice []string) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				stringSlice = append(stringSlice, a.Val)
+				break
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		stringSlice = parseNode(c, stringSlice)
+	}
+	return stringSlice
 }
