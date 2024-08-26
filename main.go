bde3da1c3ceb5dd76d979bf9525dedf2387839d3
David Shaw <dshaw11@utk.edu>
2024-08-26 16:31:23 -0400

    parse user args

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main() {
-	argsWithProg := os.Args
-	fmt.Println((argsWithProg[1]))
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(os.Args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	baseURL := os.Args[1]
+	fmt.Printf("starting crawl of: %s\n", baseURL)
 }
