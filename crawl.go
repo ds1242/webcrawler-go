e5410382c763cd421e6c887442fb4a7be4fb9ada
David Shaw <david.shaw1242@gmail.com>
2024-08-27 06:22:33 -0400

    parse and compare urls, used normalizeURL func to clean up current rawCurrentURL

---
+++ crawl.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) (map[string]int, error) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return pages, err
+	}
+
+	currentURL, err := url.Parse(rawCurrentURL)
+	if err != nil {
+		return pages, err
+	}
+
+	if baseURL.Host != currentURL.Host {
+		return pages, nil
+	}
+
+	normalizedURL, err := normalizeURL(rawCurrentURL)
+
+}
