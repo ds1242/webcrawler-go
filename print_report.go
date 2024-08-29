2a786c0996c5b47bcedd1d82327c49251414017f
David Shaw <dshaw11@utk.edu>
2024-08-29 15:48:56 -0400

    slight adjustment to names of things

--- print_report.go
+++ print_report.go
@@ -6,39 +6,40 @@ import (
 )
 
 type Page struct {
-	PageURL   string
-	LinkCount int
+	PageURL 	string
+	LinkCount	int
 }
 
 func printReport(pages map[string]int, baseURL string) {
-
+	
 	var pageSlice []Page
-
+	
 	for normalizedURL, count := range pages {
 		pageSlice = append(pageSlice, Page{
-			PageURL:   normalizedURL,
-			LinkCount: count,
+			PageURL: 	normalizedURL,
+			LinkCount: 	count,
 		})
 	}
 	sort.Sort(ByPageCountAndTitle(pageSlice))
-
+	
 	fmt.Printf(`=============================
 REPORT for %s
 =============================`, baseURL)
 	fmt.Println()
 
-	for _, page := range pageSlice {
+	for _, page := range pageSlice{
 		fmt.Printf("Found %d internal links to %s\n", page.LinkCount, page.PageURL)
 	}
 }
 
+
 type ByPageCountAndTitle []Page
 
-func (p ByPageCountAndTitle) Len() int      { return len(p) }
-func (p ByPageCountAndTitle) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
-func (p ByPageCountAndTitle) Less(i, j int) bool {
+func (p ByPageCountAndTitle) Len() int           { return len(p) }
+func (p ByPageCountAndTitle) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p ByPageCountAndTitle) Less(i, j int) bool { 
 	if p[i].LinkCount == p[j].LinkCount {
 		return p[i].PageURL < p[j].PageURL
 	}
-	return p[i].LinkCount < p[j].LinkCount
+	return p[i].LinkCount < p[j].LinkCount 
 }
